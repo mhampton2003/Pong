9c4a5e9ef0bab5cb8f04cf9a50a849c2cb90fa00
mhampton2003 <mhampton2003@gmail.com>
2024-11-12 18:35:45 -0700

    Changed LCD to display ball and platform of appropriate size

--- Pong/hardware.c
+++ Pong/hardware.c
@@ -23,9 +23,7 @@ int writeLCD(char text[])
 	void *virtual_base;
 	int fd;
 
-
 	LCD_CANVAS LcdCanvas;
-
 
 	// map the address space for the LED registers into user space so we can interact with them.
 	// we'll actually map in the entire CSR span of the HPS since we want to access various registers within that span
@@ -36,18 +34,11 @@ int writeLCD(char text[])
 
 	virtual_base = mmap( NULL, HW_REGS_SPAN, ( PROT_READ | PROT_WRITE ), MAP_SHARED, fd, HW_REGS_BASE );
 
-
-
 	if( virtual_base == MAP_FAILED ) {
 		printf( "ERROR: mmap() failed...\n" );
 		close( fd );
 		return( 1 );
 	}
-
-
-
-	//printf("Can you see LCD?(CTRL+C to terminate this program)\r\n");
-	printf("Graphic LCD Demo\r\n");
 
 	LcdCanvas.Width = LCD_WIDTH;
 	LcdCanvas.Height = LCD_HEIGHT;
@@ -69,17 +60,9 @@ int writeLCD(char text[])
 		// clear screen
 		DRAW_Clear(&LcdCanvas, LCD_WHITE);
 
-			// demo grphic api
-		DRAW_Rect(&LcdCanvas, 0,0, LcdCanvas.Width-1, LcdCanvas.Height-1, LCD_BLACK); // retangle
-		DRAW_Circle(&LcdCanvas, 10, 10, 6, LCD_BLACK);
-		DRAW_Circle(&LcdCanvas, LcdCanvas.Width-10, 10, 6, LCD_BLACK);
-		DRAW_Circle(&LcdCanvas, LcdCanvas.Width-10, LcdCanvas.Height-10, 6, LCD_BLACK);
-		DRAW_Circle(&LcdCanvas, 10, LcdCanvas.Height-10, 6, LCD_BLACK);
-
-		// demo font
-		DRAW_PrintString(&LcdCanvas, 40, 5, "I", LCD_BLACK, &font_16x16);
-		DRAW_PrintString(&LcdCanvas, 40, 5+16, "LOVE", LCD_BLACK, &font_16x16);
-		DRAW_PrintString(&LcdCanvas, 40, 5+32, "Cookies", LCD_BLACK, &font_16x16);
+		// x, y, width, height
+		DRAW_Rect(&LcdCanvas, 90, 60, 45, 55, LCD_BLACK); // rectangle
+		DRAW_Circle(&LcdCanvas, 10, 10, 4, LCD_BLACK); // circle
 
 		DRAW_Refresh(&LcdCanvas);
 
